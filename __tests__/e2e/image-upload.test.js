8e054527368e010065d4272494e7bd5eb9171742
Murat Ozcan <muratkerem@gmail.com>
2023-01-12 09:14:40 -0600

    4.16 done

---
+++ __tests__/e2e/image-upload.test.js
@@ -0,0 +1,118 @@
+// [4.9] e2e test getMyProfile [4.12] editMyProfile
+require('dotenv').config()
+const {signInUser} = require('../../test-helpers/helpers')
+const AWS = require('aws-sdk')
+const path = require('path')
+const fs = require('fs')
+const http = require('axios')
+const {axiosGraphQLQuery} = require('../../test-helpers/graphql')
+
+describe('getUploadUrl and upload an image', () => {
+  let signedInUser
+  beforeAll(async () => {
+    signedInUser = await signInUser()
+  })
+
+  it('should get an S3 url and upload an image', async () => {
+    // as a signed in user, make a request to get a signed S3 URL
+    // we can copy the query from the AppSync console,
+    // here we are taking 2 inputs as a parameters, mirroring the type at schema.api.graphql
+    // getImageUploadUrl(extension: String, contentType: String): AWSURL!
+    const getImageUploadUrl = `query getImageUploadUrl($extension: String, $contentType: String) {
+      getImageUploadUrl(extension: $extension, contentType: $contentType)
+    }`
+
+    // Make a graphQL request with the query and variables
+    const extension = '.png'
+    const contentType = 'image/png'
+    const data = await axiosGraphQLQuery(
+      process.env.API_URL,
+      signedInUser.accessToken,
+      getImageUploadUrl,
+      {extension, contentType},
+    )
+    const signedUrl = data.getImageUploadUrl
+    console.log(`[${signedInUser.username}] - got image upload url`)
+
+    const result = new RegExp(
+      `https://${process.env.BUCKET_NAME}.s3-accelerate.amazonaws.com/${
+        signedInUser.username
+      }/.*${extension || ''}?.*Content-Type=${
+        contentType ? contentType.replace('/', '%2F') : 'image%2Fjpeg'
+      }.*`,
+    )
+    // confirm that the signed url exists
+    expect(signedUrl).toMatch(result)
+
+    // upload an image
+    // get the file path of the logo.png file
+    const filePath = path.join(__dirname, '../../test-helpers/data/logo.png')
+    const fileToUpload = fs.readFileSync(filePath)
+    // make a graphQL request to upload the image
+    await http({
+      method: 'PUT',
+      url: signedUrl,
+      headers: {
+        'Content-Type': contentType,
+      },
+      data: fileToUpload,
+    })
+    console.log(`uploaded image to ${signedUrl}`)
+
+    // download the image
+    // const downloadUrl = signedUrl.split('?')[0]
+    // await http({
+    //   method: 'GET',
+    //   url: downloadUrl,
+    // })
+    // console.log(`downloaded image to ${signedUrl}`)
+
+    // might as well clean up the image
+    const S3BucketObjectKey = signedUrl
+      .split('s3-accelerate.amazonaws.com/')[1]
+      .split('?')[0]
+    const params = {
+      Bucket: process.env.BUCKET_NAME,
+      Delete: {
+        Objects: [
+          {
+            Key: S3BucketObjectKey, // required
+          },
+        ],
+      },
+    }
+    const s3 = new AWS.S3()
+    s3.deleteObjects(params, function (err, data) {
+      if (err) console.log(err, err.stack) // an error occurred
+      else {
+        console.log(data) // successful response
+        expect(data).toEqual({
+          Deleted: [
+            {
+              Key: S3BucketObjectKey,
+            },
+          ],
+          Errors: [],
+        })
+      }
+    })
+  })
+
+  afterAll(async () => {
+    // clean up DynamoDB and Cognito
+    const DynamoDB = new AWS.DynamoDB.DocumentClient()
+    await DynamoDB.delete({
+      TableName: process.env.USERS_TABLE,
+      Key: {
+        id: signedInUser.username,
+      },
+    }).promise()
+
+    await signedInUser.cognito
+      .adminDeleteUser({
+        UserPoolId: signedInUser.userPoolId,
+        Username: signedInUser.username,
+      })
+      .promise()
+  })
+})
---
+++ __tests__/integration/tweets.test.js
@@ -0,0 +1,83 @@
+// [4.16] Integration test for tweet mutation
+require('dotenv').config()
+const AWS = require('aws-sdk')
+const {signInUser} = require('../../test-helpers/helpers')
+const handler = require('../../functions/tweet').handler
+
+/**
+ * Generates an event object that can be used to test the lambda function
+ * @param {string} username - the id of the user who is tweeting
+ * @param {string} text - the text of the tweet
+ * @returns {Object} - event
+ */
+const generateTweetEvent = (username, text) => {
+  return {
+    identity: {
+      username: username,
+    },
+    arguments: {
+      text,
+    },
+  }
+}
+
+describe('Given an authenticated user', () => {
+  let signedInUser
+  beforeAll(async () => {
+    signedInUser = await signInUser()
+  })
+
+  it('should write the tweet to the Tweets, Timelines tables, and update Users table', async () => {
+    // create a mock event and feed it to the handler
+    const event = generateTweetEvent(signedInUser.username, 'Hello world!')
+    const context = {}
+    const tweet = await handler(event, context)
+
+    // verify the tables
+    const DynamoDB = new AWS.DynamoDB.DocumentClient()
+
+    const tweetsTableResp = await DynamoDB.get({
+      TableName: process.env.TWEETS_TABLE,
+      Key: {
+        id: tweet.id,
+      },
+    }).promise()
+    expect(tweetsTableResp.Item).toBeTruthy()
+
+    const timelinesTableResp = await DynamoDB.get({
+      TableName: process.env.TIMELINES_TABLE,
+      Key: {
+        userId: signedInUser.username,
+        tweetId: tweet.id,
+      },
+    }).promise()
+    expect(timelinesTableResp.Item).toBeTruthy()
+
+    const usersTableResp = await DynamoDB.get({
+      TableName: process.env.USERS_TABLE,
+      Key: {
+        id: signedInUser.username,
+      },
+    }).promise()
+    expect(usersTableResp.Item).toBeTruthy()
+    expect(usersTableResp.Item.tweetsCount).toEqual(1)
+  })
+
+  afterAll(async () => {
+    // clean up DynamoDB and Cognito
+    const DynamoDB = new AWS.DynamoDB.DocumentClient()
+    await DynamoDB.delete({
+      TableName: process.env.USERS_TABLE,
+      Key: {
+        id: signedInUser.username,
+      },
+    }).promise()
+
+    await signedInUser.cognito
+      .adminDeleteUser({
+        UserPoolId: signedInUser.userPoolId,
+        Username: signedInUser.username,
+      })
+      .promise()
+  })
+})

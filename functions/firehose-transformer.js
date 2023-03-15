680c53c066817bd3faea8d858d9a1446b84ab762
Murat Ozcan <muratkerem@gmail.com>
2023-03-15 08:08:38 -0500

    disabled cloudwatch and kinesis firehose because of cost alerts

--- functions/firehose-transformer.js
+++ functions/firehose-transformer.js
@@ -1,16 +1,17 @@
 // [93] Configure Kinesis Firehose
+// disabled Kinesis Firehose because of cost alerts
 // (93.0) use a lambda to transform the data into a Kinesis Firehose stream (it comes as a JSON object but not formatted line by line)
-module.exports.handler = async event => {
-  const output = event.records.map(record => {
-    const data = Buffer.from(record.data, 'base64').toString()
-    const newData = data + '\n'
+// module.exports.handler = async event => {
+//   const output = event.records.map(record => {
+//     const data = Buffer.from(record.data, 'base64').toString()
+//     const newData = data + '\n'
 
-    return {
-      recordId: record.recordId,
-      result: 'Ok',
-      data: Buffer.from(newData).toString('base64'),
-    }
-  })
+//     return {
+//       recordId: record.recordId,
+//       result: 'Ok',
+//       data: Buffer.from(newData).toString('base64'),
+//     }
+//   })
 
-  return {records: output}
-}
+//   return {records: output}
+// }

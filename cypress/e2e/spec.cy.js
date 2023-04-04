7008cfd87023bdbb544d00862813d957a4b3ab57
Murat Ozcan <muratkerem@gmail.com>
2023-04-04 08:21:09 -0500

    pass PR

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,10 +4,7 @@ describe('template spec', () => {
   it('passes', () => {
     // log out environment variables
 
-    cy.exec('git rev-parse --abbrev-ref HEAD')
-      .its('stdout')
-      .print()
-      .should('eq', Cypress.env('STAGE'))
+    cy.print(Cypress.env('STAGE'))
     expect(Cypress.env('AWS_NODEJS_CONNECTION_REUSE_ENABLED')).to.eq('1')
     expect(Cypress.env('AWS_REGION')).to.eq('eu-west-1')
   })

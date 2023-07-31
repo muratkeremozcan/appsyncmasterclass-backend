22f7eeeb9e3cecc455b362cd465984c0e1124782
Murat Ozcan <muratkerem@gmail.com>
2023-07-31 14:47:49 -0500

    try better cognito sign up

--- test-helpers/cognito.js
+++ test-helpers/cognito.js
@@ -1,5 +1,11 @@
 require('dotenv').config()
 const AWS = require('aws-sdk')
+const {
+  CognitoIdentityProviderClient,
+  AdminCreateUserCommand,
+  AdminInitiateAuthCommand,
+  AdminRespondToAuthChallengeCommand,
+} = require('@aws-sdk/client-cognito-identity-provider')
 const chance = require('chance').Chance()
 
 /**
@@ -87,18 +93,104 @@ const authorizeUser = async ({
     .promise()
 }
 
+// /**
+//  * Signs up a user, signs in that user and returns an authenticated user
+//  * @returns {Object} - {username, name, email, idToken, accessToken}
+//  */
+// const signInUser = async () => {
+//   const {name, email, password, username, cognito, clientId, userPoolId} =
+//     await signUpUser()
+
+//   // authorize the user
+//   const {
+//     AuthenticationResult: {IdToken, AccessToken},
+//   } = await authorizeUser({username, password, clientId})
+
+//   return {
+//     username,
+//     name,
+//     email,
+//     userPoolId,
+//     cognito,
+//     idToken: IdToken,
+//     accessToken: AccessToken,
+//   }
+// }
+
+// needs number, special char, upper and lower case
+const random_password = () => `${chance.string({length: 8})}B!gM0uth`
+
 /**
- * Signs up a user, signs in that user and returns an authenticated user
- * @returns {Object} - {username, name, email, idToken, accessToken}
+ * Creates an authenticated user.
+ *
+ * @async
+ * @returns {Promise<Object>} A promise that resolves to an object containing user details:
+ *    - username: a string containing the username of the newly created user.
+ *    - firstName: a string containing the first name of the newly created user.
+ *    - lastName: a string containing the last name of the newly created user.
+ *    - idToken: a string containing the ID token for the newly authenticated user.
+ * @throws {Error} Throws an error if there is a problem creating the user or authenticating.
  */
 const signInUser = async () => {
-  const {name, email, password, username, cognito, clientId, userPoolId} =
-    await signUpUser()
+  // @ts-expect-error - If you are running this in an AWS environment (like Lambda, EC2, ECS), the SDK will automatically load the argument
+  // an object with AWS_REGION, AWS_ACCESS_KEY_ID, and AWS_SECRET_ACCESS_KEY.
+  const cognito = new CognitoIdentityProviderClient()
 
-  // authorize the user
-  const {
-    AuthenticationResult: {IdToken, AccessToken},
-  } = await authorizeUser({username, password, clientId})
+  const userPoolId = process.env.cognito_user_pool_id
+  // const clientId = process.env.cognito_server_client_id // replaced with the below as of SNS & EventBridge in e2e tests > Capture CloudFormation outputs in .env file
+  const clientId =
+    process.env.CognitoUserPoolServerClientId ||
+    process.env.cognito_server_client_id
+
+  const firstName = chance.first({nationality: 'en'})
+  const lastName = chance.last({nationality: 'en'})
+  const suffix = chance.string({length: 8, pool: 'abcdefghijklmnopqrstuvwxyz'})
+  const username = `test-${firstName}-${lastName}-${suffix}`
+  const password = random_password()
+  const email = `${firstName}-${lastName}@big-mouth.com`
+  const name = `${firstName} ${lastName} ${suffix}`
+
+  const createReq = new AdminCreateUserCommand({
+    UserPoolId: userPoolId,
+    Username: username,
+    MessageAction: 'SUPPRESS',
+    TemporaryPassword: password,
+    UserAttributes: [
+      {Name: 'given_name', Value: firstName},
+      {Name: 'family_name', Value: lastName},
+      {Name: 'email', Value: email},
+    ],
+  })
+  await cognito.send(createReq)
+
+  console.log(`[${username}] - user is created`)
+
+  const req = new AdminInitiateAuthCommand({
+    AuthFlow: 'ADMIN_NO_SRP_AUTH',
+    UserPoolId: userPoolId,
+    ClientId: clientId,
+    AuthParameters: {
+      USERNAME: username,
+      PASSWORD: password,
+    },
+  })
+  const resp = await cognito.send(req)
+
+  console.log(`[${username}] - initialised auth flow`)
+
+  const challengeReq = new AdminRespondToAuthChallengeCommand({
+    UserPoolId: userPoolId,
+    ClientId: clientId,
+    ChallengeName: resp.ChallengeName,
+    Session: resp.Session,
+    ChallengeResponses: {
+      USERNAME: username,
+      NEW_PASSWORD: random_password(),
+    },
+  })
+  const challengeResp = await cognito.send(challengeReq)
+
+  console.log(`[${username}] - responded to auth challenge`)
 
   return {
     username,
@@ -106,8 +198,11 @@ const signInUser = async () => {
     email,
     userPoolId,
     cognito,
-    idToken: IdToken,
-    accessToken: AccessToken,
+    idToken: challengeResp.AuthenticationResult.IdToken,
+    accessToken: challengeResp.AuthenticationResult.AccessToken,
+    firstName,
+    lastName,
+    clientId,
   }
 }
 
